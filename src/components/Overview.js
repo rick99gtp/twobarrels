da54995205d9c1e69be0dad200db517368aaaf76
Rick Beyer <beyer.richard73@gmail.com>
2021-10-15 16:46:22 -0500

    Starting the Overview page

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,15 +1,24 @@
 import './Overview.css';
 import { useEffect } from 'react';
+import background from '../assets/2B-building-v5.jpg';
 
-const Overview= (props) => {
+const Overview = (props) => {
 
     useEffect(() => {
         props.changeActivePage(0);
-    },[]);
+    }, []);
 
     return (
         <div className='overview'>
-            Overview page
+            <section>
+                <div className='overview__hero'  style={{ backgroundImage: `url(${background})` }}>
+                    <h1>We Give'Em Two Barrels - Every Day</h1>
+                    <hr />
+                    <span className='overview__hero-bottom'>
+                        Proudly located in Spokane, WA & Austin, TX
+                    </span>
+                </div>
+            </section>
         </div>
     )
 }
