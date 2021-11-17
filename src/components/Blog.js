8b553e98af9f2d2468d8cdb0f6af6953c48801d1
Rick Beyer <beyer.richard73@gmail.com>
2021-11-17 11:13:41 -0600

    Setup blog page with h1

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,8 +2,8 @@ import './Blog.css';
 
 const Blog = () => {
     return (
-        <div>
-            Blog page
+        <div className='blog__container'>
+            <h1 className='blog-h1'>Latest Posts</h1>
         </div>
     )
 }
