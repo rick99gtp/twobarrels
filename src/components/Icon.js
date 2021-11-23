dddb6c576849774458b123703c980e9435011a13
Rick Beyer <beyer.richard73@gmail.com>
2021-11-23 10:52:00 -0600

    Changed hover color of icons on main page to white

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,15 +1,23 @@
 import './Icon.css';
+// import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Icon = (props) => {
 
-    const linkStyle = {
-        textDecoration: 'none'
-    };
+// const [color, setColor] = useState('var(--turquoise');
+
+// const iconStyle = {
+//     textDecoration: "none",
+//     color: color
+// }
+
+// useEffect(() => {
+//     console.log(color);
+// }, [color]);
 
 return (
     <div className='icon'>
-        <Link to='/jobs' style={linkStyle}>
+        <Link to='/jobs'>
             {props.icon}
             <h4 className='icon__h4'>{props.h4}</h4>
             <p className='icon__p'>{props.p}</p>
--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,35 +1,32 @@
 import './Icons.css';
 import Icon from './Icon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { HiCog } from 'react-icons/hi';
-// import { RiCodeSSlashFill } from 'react-icons/ri';
-// import { FaPencilAlt } from 'react-icons/fa';
 
 const Icons = () => {
 
     const iconStyle = {
         fontSize: '36px',
-        color: 'var(--turquoise)',
         overflow: 'hidden',
         transition: 'all .25s',
-        marginTop: '30px'
+        marginTop: '30px',
+        textDecoration: "none"
     };
 
     return (
         <div className='icons'>
             <h2></h2>
             <Icon
-                icon={<FontAwesomeIcon icon="cog" style={iconStyle}/>}
+                icon={<FontAwesomeIcon icon="cog" style={iconStyle} className='icon-hover-style'/>}
                 h4="Systems"
-                p="Are You Passionate About Turning Things Off And On Again? Are You Naturally Tech-Savvy? Let's Talk!"/>
+                p="Are You Passionate About Turning Things Off And On Again? Are You Naturally Tech-Savvy? Let's Talk!" />
             <Icon
-                icon={<FontAwesomeIcon icon="code" style={iconStyle}/>}
+                icon={<FontAwesomeIcon icon="code" style={iconStyle} className='icon-hover-style' />}
                 h4="Software"
-                p="Got Code? Is It Logical And Clean? Do You Feel Passionate About Tabs Or Spaces? If So, We Want Your Skills!"/>
+                p="Got Code? Is It Logical And Clean? Do You Feel Passionate About Tabs Or Spaces? If So, We Want Your Skills!" />
             <Icon
-                icon={<FontAwesomeIcon icon="pencil-alt" style={iconStyle}/>}
+                icon={<FontAwesomeIcon icon="pencil-alt" style={iconStyle} className='icon-hover-style' />}
                 h4="Creative"
-                p="Are You Smart? A Little Competitve? Do You Care About Sentences And The Way A Page Looks? Talk To Us!"/>
+                p="Are You Smart? A Little Competitve? Do You Care About Sentences And The Way A Page Looks? Talk To Us!" />
         </div>
     )
 }
