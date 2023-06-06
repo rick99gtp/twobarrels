ecc5914fcb4f40925aa7fdb8e024437b4d17042c
rlnB2022 <beyer.richard73@gmail.com>
2023-06-06 15:09:18 -0500

    Corrected size of Hero h1 and sub-text

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
-import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { faTwitterSquare, faFacebookSquare, faTumblrSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { useEffect } from 'react/cjs/react.development';
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import {
+	faTwitterSquare,
+	faFacebookSquare,
+	faTumblrSquare,
+	faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
+	library.add(
+		fab,
+		fas,
+		faTwitterSquare,
+		faFacebookSquare,
+		faTumblrSquare,
+		faLinkedin
+	);
 
-  library.add(fab, fas, faTwitterSquare, faFacebookSquare, faTumblrSquare, faLinkedin);
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [width, setWidth] = useState(window.innerWidth);
 
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [width, setWidth] = useState(window.innerWidth);
+	const changeMenuOpen = () => {
+		setMenuOpen(!menuOpen);
+	};
 
-  const changeMenuOpen = () => {
-      setMenuOpen(!menuOpen);
-  };
+	useEffect(() => {
+		const handleResizeWindow = () => setWidth(window.innerWidth);
 
-  useEffect(() => {
-    const handleResizeWindow = () => setWidth(window.innerWidth);
+		window.addEventListener("resize", handleResizeWindow);
+		return () => {
+			window.removeEventListener("resize", handleResizeWindow);
+		};
+	}, []);
 
-    window.addEventListener('resize', handleResizeWindow);
-    return () => {
-      window.removeEventListener('resize', handleResizeWindow);
-    };
-
-  }, []);
-
-  return (
-
-    <div className="App">
-      <Router>
-        <Header menuOpen={menuOpen} changeMenuOpen={changeMenuOpen} appWidth={width}/>
-        <Main appWidth={width}/>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Header
+					menuOpen={menuOpen}
+					changeMenuOpen={changeMenuOpen}
+					appWidth={width}
+				/>
+				<Main appWidth={width} />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
