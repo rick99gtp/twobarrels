9b52c0ee85c50c85235a1f5c8d27be9f861b89ca
rlnB2022 <beyer.richard73@gmail.com>
2023-06-19 08:45:51 -0500

    Removed another props and replaced with destructured value

--- src/components/ExitMobile/ExitMobile.js
+++ src/components/ExitMobile/ExitMobile.js
@@ -2,11 +2,11 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./ExitMobile.css";
 
-const ExitMobile = (props) => {
+const ExitMobile = ({ changeMenuOpen, menuOpen }) => {
 	return (
 		<div
-			onClick={() => props.changeMenuOpen()}
-			className={`exit-mobile ${props.menuOpen ? "show-exit-mobile" : ""}`}
+			onClick={() => changeMenuOpen()}
+			className={`exit-mobile ${menuOpen ? "show-exit-mobile" : ""}`}
 		>
 			<FontAwesomeIcon icon="times" />
 		</div>
