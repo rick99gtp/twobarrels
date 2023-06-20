8889dec2e14afc568d94b6663d27e9aefcd5a717
rlnB2022 <beyer.richard73@gmail.com>
2023-06-20 13:36:28 -0500

    Switch back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import { useEffect, useState } from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
